cdc7f408c26262c52ffdd5b98d9af056b233acd4
NoKidS <112279335+Nokids@users.noreply.github.com>
2023-11-19 21:11:04 +0700

    first commit

---
+++ Tugas Uas semester 3- Normalisasi/script.js
@@ -0,0 +1,16 @@
+const navBar = document.querySelector(".navbar");
+let prevScrollPos = window.scrollY;
+
+window.addEventListener("scroll", function () {
+  let currScrollPos = window.scrollY;
+
+  if (currScrollPos > prevScrollPos) {
+    navBar.style.transform = `translateY(-105%)`;
+  } else {
+    navBar.style.transform = `translateY(0%)`;
+  }
+
+  prevScrollPos = currScrollPos;
+});
+
+
